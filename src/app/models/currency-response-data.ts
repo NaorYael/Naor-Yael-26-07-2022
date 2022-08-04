6ffecf2013e0755969de1243333b85e92a688a62
Naor Yael <47955339+NaorYael@users.noreply.github.com>
2022-08-04 10:20:02 +0300

    fix warning "depends on 'moment'. CommonJS or AMD dependencies can cause optimization bailouts."

--- src/app/models/currency-response-data.ts
+++ src/app/models/currency-response-data.ts
@@ -1,8 +1,8 @@
 export interface CurrencyResponseData {
-    "base": string,
-    "target": string,
-    "base_amount": number,
-    "converted_amount": number,
-    "exchange_rate": number,
-    "date": string
+    base: string,
+    target: string,
+    base_amount: number,
+    converted_amount: number,
+    exchange_rate: number,
+    date: string
 }
